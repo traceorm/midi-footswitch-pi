1615126f92efa61b34bf57a84f6f89335f260caf
traceorm <m.kretsche@googlemail.com>
2016-12-25 20:36:55 +0100

    Code aufgeräumt, Trigger auf beide Flanken gesetzt.

--- midiboard.py
+++ midiboard.py
@@ -25,9 +25,6 @@ S4_B = 18
 #panic button to end programm // wichtig weil hardware gesteuert
 PANIC = 8
 
-
-
-
 #Setting up GPIO
 #top line
 GPIO.setup(S0_T, GPIO_IN, pull_up_down=GPIO.PUD_UP)
@@ -50,12 +47,13 @@ GPIO.setup(PANIC, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 midi = GrandmeisterMIDI(0, 'USB')
 
+#set startbar
 currentBar  = 1 
 #variablen die, die aktuelle Funktionen der Schalter vorhalten
 #die idee ist im moment alle funktionen die initiert werden sollen als array zu speichern und dann in den HIT-Funktionen alle befehle aus zuführen
 #Funktionen sollen aus xml datei geladen werden
 currentFunctions = {
-	"T1" : [0,[1,2],0], 
+	"T1" : [0,[1,2],0],
 	"T2" : [0,0,0],
 	"T3" : [0,0,0],
 	"T4" : [0,0,0],
@@ -182,50 +180,6 @@ barTooBarFunctions = {
 }
 
 
-### Methoden, welche das Senden aufrufen
-## werden ggf einen switch / elifchain brauchen für die currentBar    
-
-#top buttons
-def s0_tHit():
-	midi.functionResolver(currentFunctions[1])
-    #do midi stuff here
-def s1_tHit():
-    midi.functionResolver(currentFunctions[2])
-def s2_tHit():
-    #do midi stuff here
-    midi.functionResolver(currentFunctions[3])
-def s3_tHit():
-    #do midi stuff here
-    midi.functionResolver(currentFunctions[4])
-def s4_tHit():
-    #kein midi stuff hier, nur inkrementieren der currentBar     variable und aufrufen der led-funktion.
-    currentBar     += 1
-    if(currentBar>8):
-    	currentBar = 1
-    currentFunctions = barTooBarFunctions[currentBar]	
-    setLEDBar()
-
-#bottom buttons
-def s0_bHit():
-    #do midi stuff here
-    midi.functionResolver(currentFunctions[5])
-def s1_bHit():
-    #do midi stuff here
-    midi.functionResolver(currentFunctions[6])
-def s2_bHit():
-    #do midi stuff here
-    midi.functionResolver(currentFunctions[7])
-def s3_bHit():
-    #do midi stuff here
-    midi.functionResolver(currentFunctions[8])
-def s4_bHit():
-    #kein midi stuff hier, nur inkrementieren der currentBar     variable und aufrufen der led-funktion.
-    currentBar     -= 1
-    if(currentBar<1):
-    	currentBar = 8
-    setLEDBar()
-
-
 def setLEDBar():
 	## hier leds in abhängigkeit der currentBar setzen.
 
@@ -234,74 +188,51 @@ def setLEDBar():
 #since we got switches but want a button behavior / trigger on every change
 
 #top line switch interrupts
-def s0_tFalling(channel):
-    s0_tHit()
-def s0_tRising(chanel):
-    s0_tHit()
-def s1_tFalling(channel):
-    s1_tHit()
-def s1_tRising(chanel):
-    s1_tHit()
-def s2_tFalling(channel):
-    s2_tHit()
-def s2_tRising(chanel):
-    s2_tHit()
-def s3_tFalling(channel):
-    s3_tHit()
-def s3_tRising(chanel):
-    s3_tHit()
-def s4_tFalling(channel):
-    s4_tHit()
-def s4_tRising(chanel):
-    s4_tHit()
+def s0_tBoth(channel):
+	midi.functionResolver(currentFunctions[1])
+def s1_tBoth(channel):
+	midi.functionResolver(currentFunctions[2])
+def s2_tBoth(channel):
+	midi.functionResolver(currentFunctions[3])
+def s3_tBoth(channel):
+	midi.functionResolver(currentFunctions[4])
+def s4_tBoth(channel):
+	currentBar+= 1
+	if(currentBar>8):
+		currentBar = 1
+	currentFunctions = barTooBarFunctions[currentBar]	
+	setLEDBar()
 
 #bottom line switch interrupts
-def s0_bFalling(channel):
-    s0_bHit()
-def s0_bRising(chanel):
-    s0_bHit()
-def s1_bFalling(channel):
-    s1_bHit()
-def s1_bRising(chanel):
-    s1_bHit()
-def s2_bFalling(channel):
-    s2_bHit()
-def s2_bRising(chanel):
-    s2_bHit()
-def s3_bFalling(channel):
-    s3_bHit()
-def s3_bRising(chanel):
-    s3_bHit()
-def s4_bFalling(channel):
-    s4_bHit()
-def s4_bRising(chanel):
-    s4_bHit()
+def s0_bBoth(chanel):
+	midi.functionResolver(currentFunctions[5])
+def s1_bBoth(chanel):
+	midi.functionResolver(currentFunctions[6])
+def s2_bBoth(chanel):
+	midi.functionResolver(currentFunctions[7])
+def s3_bBoth(chanel):
+	midi.functionResolver(currentFunctions[8])
+def s4_bBoth(chanel):
+	currentBar     -= 1
+	if(currentBar<1):
+		currentBar = 8
+	setLEDBar()
 
 #define interrupts at GPIO
 
 #top line interrupts
-GPIO.add_event_detect(S0_T, GPIO.FALLING, callback=s0_tFalling, bouncetime=300)
-GPIO.add_event_detect(S0_T, GPIO.RISING, callback=s0_tRising, bouncetime=300)
-GPIO.add_event_detect(S1_T, GPIO.FALLING, callback=s1_tFalling, bouncetime=300)
-GPIO.add_event_detect(S1_T, GPIO.RISING, callback=s1_tRising, bouncetime=300)
-GPIO.add_event_detect(S2_T, GPIO.FALLING, callback=s2_tFalling, bouncetime=300)
-GPIO.add_event_detect(S2_T, GPIO.RISING, callback=s2_tRising, bouncetime=300)
-GPIO.add_event_detect(S3_T, GPIO.FALLING, callback=s3_tFalling, bouncetime=300)
-GPIO.add_event_detect(S3_T, GPIO.RISING, callback=s3_tRising, bouncetime=300)
-GPIO.add_event_detect(S4_T, GPIO.FALLING, callback=s4_tFalling, bouncetime=300)
-GPIO.add_event_detect(S4_T, GPIO.RISING, callback=s4_tRising, bouncetime=300)
+GPIO.add_event_detect(S0_T, GPIO.BOTH, callback=s0_tBoth, bouncetime=300)
+GPIO.add_event_detect(S1_T, GPIO.BOTH, callback=s1_tBoth, bouncetime=300)
+GPIO.add_event_detect(S2_T, GPIO.BOTH, callback=s2_tBoth, bouncetime=300)
+GPIO.add_event_detect(S3_T, GPIO.BOTH, callback=s3_tBoth, bouncetime=300)
+GPIO.add_event_detect(S4_T, GPIO.BOTH, callback=s4_tBoth, bouncetime=300)
 
 #bottom line interrupts
-GPIO.add_event_detect(S0_B, GPIO.FALLING, callback=s0_bFalling, bouncetime=300)
-GPIO.add_event_detect(S0_B, GPIO.RISING, call(back=s0_bRising, bouncetime=300)
-GPIO.add_event_detect(S1_B, GPIO.FALLING, callback=s1_bFalling, bouncetime=300)
-GPIO.add_event_detect(S1_B, GPIO.RISING, callback=s1_bRising, bouncetime=300)
-GPIO.add_event_detect(S2_B, GPIO.FALLING, callback=s2_bFalling, bouncetime=300)
-GPIO.add_event_detect(S2_B, GPIO.RISING, callback=s2_bRising, bouncetime=300)
-GPIO.add_event_detect(S3_B, GPIO.FALLING, callback=s3_bFalling, bouncetime=300)
-GPIO.add_event_detect(S3_B, GPIO.RISING, callback=s3_bRising, bouncetime=300)
-GPIO.add_event_detect(S4_B, GPIO.FALLING, callback=s4_bFalling, bouncetime=300)
-GPIO.add_event_detect(S4_B, GPIO.RISING, callback=s4_bRising, bouncetime=300)
+GPIO.add_event_detect(S0_B, GPIO.BOTH, callback=s0_bBoth, bouncetime=300)
+GPIO.add_event_detect(S1_B, GPIO.BOTH, callback=s1_bBoth, bouncetime=300)
+GPIO.add_event_detect(S2_B, GPIO.BOTH, callback=s2_bBoth, bouncetime=300)
+GPIO.add_event_detect(S3_B, GPIO.BOTH, callback=s3_bBoth, bouncetime=300)
+GPIO.add_event_detect(S4_B, GPIO.BOTH, callback=s4_bBoth, bouncetime=300)
 
 
 
